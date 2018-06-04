6fa62d38c6a183f83785b3c49e05f491ff76c680
YounesN <younes@wayne.edu>
2018-06-04 19:03:59 -0400

    checking for input file and ensemble keyword

--- src/GOMC.cpp
+++ src/GOMC.cpp
@@ -1,24 +1,35 @@
 #include "InputFileReader.h"
 #include <string>
 #include <stdlib.h>
+#include <iostream>
 
 std::string EnsembleSearch(std::string filename);
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char* argv[])
+{
+  if(argc < 2) {
+    std::cerr << "Error: Input file name is required!" << std::endl;
+    std::cerr << "Example: GOMC in.conf" << std::endl;
+    exit(0);
+  }
   std::string filename(argv[1]);
 	std::string ensembleType = EnsembleSearch(filename);
 
-	std::string Executable_To_Run = "./GOMC_CPU_";
-	Executable_To_Run += ensembleType;
-  Executable_To_Run += " ";
-  Executable_To_Run += filename;
+  if(ensembleType == "NAN") {
+    std::cerr << "Error: Ensemble type is required in your input file!" << std::endl;
+    std::cerr << "Example: Ensemble GCMC" << std::endl;
+    exit(0);
+  }
 
 #ifdef _WIN32
 	// TODO
 #endif
 
 #if defined(__linux__) || defined(__APPLE__)
+	std::string Executable_To_Run = "./GOMC_CPU_";
+	Executable_To_Run += ensembleType;
+  Executable_To_Run += " ";
+  Executable_To_Run += filename;
 	system(Executable_To_Run.c_str());
 #endif
 	
