b647929d74eb87d172a32ef0e26f49d73b8c38c4
GarrettLong <longge@miamioh.edu>
2018-06-26 09:59:18 -0400

    Added parentheses to nmin/max if statement

--- src/MoleculeTransfer.h
+++ src/MoleculeTransfer.h
@@ -165,8 +165,8 @@ inline void MoleculeTransfer::Accept(const uint rejectState, const uint step)
 		double acceptance = molTransCoeff * Wrat;
 		#if	ENSEMBLE == GCMC
 			//Ensure no insertion proceeds past Nmax or deletion past Nmin (Nmin not enforced yet)
-			if (transitionMatrixRef.biasingOn && (sourceBox == mv::BOX1 && molLookRef.NumKindInBox(transitionMatrixRef.molKind, mv::BOX0) >= transitionMatrixRef.nmax) 
-				|| (sourceBox == mv::BOX0 && molLookRef.NumKindInBox(transitionMatrixRef.molKind, mv::BOX1) <= transitionMatrixRef.nmin)) 
+			if (transitionMatrixRef.biasingOn && ((sourceBox == mv::BOX1 && molLookRef.NumKindInBox(transitionMatrixRef.molKind, mv::BOX0) >= transitionMatrixRef.nmax) 
+				|| (sourceBox == mv::BOX0 && molLookRef.NumKindInBox(transitionMatrixRef.molKind, mv::BOX1) <= transitionMatrixRef.nmin))) 
 			{
 				acceptance = 0.0;
 			}
